b52b589301d4313caba70405bc2e0a674a10ac05
sarveshmishra83 <sarveshmishra2022@gmail.com>
2024-06-28 19:26:18 +0530

    Update streamlit_app.py
    Added fruivice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 # Import python packages
 import streamlit as st
+import requests // lession 10
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 # Write directly to the app
 st.title(":cup_with_straw: Example Streamlit App :cup_with_straw:")
